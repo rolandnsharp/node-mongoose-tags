bcd82a1458243f002ab8132237c1ce83aae1881a
Roland Sharp <rolandnsharp@gmail.com>
2015-08-12 09:52:53 +1000

    Add removeTags Static

--- index.js
+++ index.js
@@ -25,6 +25,26 @@ module.exports = function(schema, options) {
     });
   };
 
+  schema.statics.removeTags = function(conditions, tags, callback) {
+
+    var update = {
+      $pullAll: {
+        tags: tags
+      }
+    };
+
+    var options = {
+      multi: true
+    };
+
+    this.update(conditions, update, options, function(err, numAffected) {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, numAffected);
+    });
+  };
+
 
   schema.methods.addTags = function(tag, callback) {
 
