ac168f82777787121946fb4cb0bf82600c860d35
Roland Sharp <rolandnsharp@gmail.com>
2015-08-12 09:45:16 +1000

    Add push all and multi mongoose options.

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ module.exports = function(schema, options) {
     tags: [String]
   });
 
-  schema.statics.addTag = function(tag, callback) {
+  schema.statics.addTags = function(conditions, tags, callback) {
+
+    var update = {
+      $pushAll: {
+        tags: tags
+      }
+    };
+
+    var options = {
+      multi: true
+    };
 
     this.update(conditions, update, options, function(err, numAffected) {
       if (err) {
@@ -16,8 +26,11 @@ module.exports = function(schema, options) {
   };
 
 
-  schema.methods.addTag = function(tag, callback) {
+  schema.methods.addTags = function(tag, callback) {
 
+
+
+    this.tags
   };
 
   schema.statics.removeTag = function(tag, callback) {
