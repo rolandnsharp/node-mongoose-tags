dee373f80be9e04fd2078282c50ddaac740ed38f
Roland Sharp <rolandnsharp@gmail.com>
2015-08-13 22:57:15 +1000

    use addToSet $each to pass idempotency test
    replace $pushAll

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ module.exports = function(schema, options) {
     };
 
     var update = {
-      $pushAll: {
-        tags: tags
+      '$addToSet': {
+        tags: { "$each": tags }
       }
     };
 
